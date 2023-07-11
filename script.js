88cabf21b95e34e5e909e4137a90dc4e28963964
Maha Abdelnabi <mahaomar@gmail.com>
2023-07-10 23:10:07 -0400

    create list item to show the dice roll history

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const buttonEl = document.getElementById("roll-button");
 const diceEl = document.getElementById("dice");
+const rollHistoryEl = document.getElementById("roll-history");
+
+
+//empty array,that's going to be updated every ttime we click the button
+let historyList = [];
 
 //func to create random number
-function rollDice(){
-    const randomNumber= Math.floor(Math.random() *6) + 1;//+1 is to get the numbers between 1-6 otherwise it gets nums btwn 0-5
-    console.log(randomNumber)
-  
-    const diceFace = getDiceFace(randomNumber);
-    console.log(diceFace)
+function rollDice() {
+  const randomNumber = Math.floor(Math.random() * 6) + 1; //+1 is to get the numbers between 1-6 otherwise it gets nums btwn 0-5
+  console.log(randomNumber);
+
+  const diceFace = getDiceFace(randomNumber);
+  diceEl.innerHTML = diceFace;
+  historyList.push(randomNumber);
+  updateRollHistory();
 }
+
+function updateRollHistory() {
+  rollHistoryEl.innerHTML = "";//make it empty at first
+  for (let i = 0; i < historyList.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `Roll ${i + 1}: <span>${getDiceFace(
+      historyList[i]
+    )}</span>`;
+    rollHistoryEl.appendChild(listItem);
+  }
+}
+
 //based on that num,create a func to change dice face
 function getDiceFace(randomNumber){
     switch (randomNumber) {
