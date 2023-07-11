504a1d2f9501d90a1e0100f46eb84b34f268e834
Maha Abdelnabi <mahaomar@gmail.com>
2023-07-10 22:56:43 -0400

    add func to change the dice face

--- script.js
+++ script.js
@@ -5,8 +5,30 @@ const diceEl = document.getElementById("dice");
 function rollDice(){
     const randomNumber= Math.floor(Math.random() *6) + 1;//+1 is to get the numbers between 1-6 otherwise it gets nums btwn 0-5
     console.log(randomNumber)
+  
+    const diceFace = getDiceFace(randomNumber);
+    console.log(diceFace)
 }
+//based on that num,create a func to change dice face
+function getDiceFace(randomNumber){
+    switch (randomNumber) {
+      case 1:
+        return "&#9856;";
+      case 2:
+        return "&#9857;";
+      case 3:
+        return "&#9858;";
+      case 4:
+        return "&#9859;";
+      case 5:
+        return "&#9860;";
+      case 6:
+        return "&#9861;";
+      default:
+        return "";
+    }
 
+}
 // by clicking the button, the dice will roll, by creating class name to the dice,and style it by css
 buttonEl.addEventListener("click", ()=>{
     diceEl.classList.add("roll-animation");
